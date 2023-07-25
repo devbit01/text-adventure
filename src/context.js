6ad8044674d9b55b1819ffe74d5fa9017a039a97
devbit01 <devbit01@gmail.com>
2023-07-25 09:17:28 -0500

    clean up files

--- src/context.js
+++ src/context.js
@@ -1,96 +1,91 @@
-import React, { useState, useContext, useReducer, useEffect } from "react";
-import { reducer } from "./reducer";
-import { synonyms, stopWords, synonmys_map } from "./data";
-import { rooms } from "./games/zorkGameData";
+import React, { useState, useContext, useReducer, useEffect } from 'react'
+import { reducer } from './reducer'
+import { synonyms, synonmys_map } from './data'
+import { rooms } from './games/zorkGameData'
 
-const AppContext = React.createContext();
+const AppContext = React.createContext()
 
 const defaultState = {
-  commands: [],
-  rooms: rooms,
-  synonyms: synonyms,
-  stopWords: stopWords,
-  here: "NORTH",
-};
+    commands: [],
+    rooms: rooms,
+    synonyms: synonyms,
+    here: 'NORTH',
+}
 
 const AppProvider = ({ children }) => {
-  const [synonymLookup, setSynonymLookup] = useState(new Map());
-  const [commandHistory, setCommandHistory] = useState([]);
-  const [outputHistory, setOutputHistory] = useState([]);
+    const [synonymLookup, setSynonymLookup] = useState(new Map())
+    const [commandHistory, setCommandHistory] = useState([])
+    const [outputHistory, setOutputHistory] = useState([])
 
-  const tell = (message, newLine = false) => {
-    setOutputHistory((previousState) => {
-      return [...previousState, message];
-    });
-    if (newLine) {
-      setOutputHistory((previousState) => {
-        return [...previousState, ""];
-      });
+    const tell = (message, newLine = false) => {
+        setOutputHistory((previousState) => {
+            return [...previousState, message]
+        })
+        if (newLine) {
+            setOutputHistory((previousState) => {
+                return [...previousState, '']
+            })
+        }
     }
-  };
 
-  const handleClearOutput = () => {
-    setOutputHistory([]);
-    return true;
-  };
-
-  const handleInput = (input) => {
-    console.log("at index:", "AT".indexOf(" "));
-
-    input = input.toUpperCase();
-    tell(">" + input);
-    setCommandHistory([...commandHistory, input]);
-    const punctuations = [".", ",", "!", "?"];
-
-    // remove punctuations and remove "stop" words
-    const cleanedInput = cleanInput(input, punctuations);
-
-    for (let word of cleanedInput.split(" ")) {
-      if (!synonymLookup.has(word)) {
-        tell(`I don't know the word "${word}".`, true);
-        return false;
-      }
-
-      const lookup = synonymLookup.get(word);
-      tell(`type: ${lookup.type}, keyword: ${lookup.keyWord}`, true);
-
-      if (lookup.keyWord === "CLEAR") {
-        handleClearOutput();
-      }
+    const handleClearOutput = () => {
+        setOutputHistory([])
+        return true
     }
-  };
 
-  useEffect(() => {
-    // initial load only
-    console.log("loading game data....");
-    console.log("loading synonyms");
-    setSynonymLookup(synonmys_map());
-  }, []);
+    const handleInput = (input) => {
+        input = input.toUpperCase()
+        tell('>' + input)
+        setCommandHistory([...commandHistory, input])
 
-  return (
-    <AppContext.Provider
-      value={{
-        handleInput,
-        outputHistory,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
-};
+        const punctuations = ['.', ',', '!', '?']
+        const stopWords = ['THE', 'A', 'AN']
+
+        // remove punctuations and remove "stop" words
+        const cleanedInput = input
+            .split('')
+            .filter((char) => !punctuations.includes(char))
+            .join('')
+            .split(' ')
+            .filter((word) => !stopWords.includes(word))
+            .join(' ')
+
+        for (let word of cleanedInput.split(' ')) {
+            if (!synonymLookup.has(word)) {
+                tell(`I don't know the word "${word}".`, true)
+                return false
+            }
+
+            const lookup = synonymLookup.get(word)
+            tell(`type: ${lookup.type}, keyword: ${lookup.keyWord}`, true)
+
+            if (lookup.keyWord === 'CLEAR') {
+                handleClearOutput()
+            }
+        }
+    }
+
+    useEffect(() => {
+        // initial load only
+        console.log('loading game data....')
+        console.log('loading synonyms')
+        setSynonymLookup(synonmys_map())
+    }, [])
+
+    return (
+        <AppContext.Provider
+            value={{
+                handleInput,
+                outputHistory,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+}
 
 export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
-
-export { AppContext, AppProvider };
-
-function cleanInput(input, punctuations) {
-  return input
-    .split("")
-    .filter((char) => !punctuations.includes(char))
-    .join("")
-    .split(" ")
-    .filter((word) => !stopWords.includes(word))
-    .join(" ");
+    return useContext(AppContext)
 }
+
+export { AppContext, AppProvider }
--- src/data.js
+++ src/data.js
@@ -16,107 +16,105 @@
 // ];
 
 export const synonyms = [
-  { type: "VERB", words: "CLEAR CLR WIPE" },
-  { type: "VERB", words: "HELLO HI YO HOWDY" },
-  { type: "VERB", words: "YELL SHOUT" },
-  { type: "VERB", words: "LOOK L" },
-  { type: "NOUN", words: "LAMP LIGHT LANTERN" },
-  { type: "PREP", words: "AT TO" },
-  { type: "PREP", words: "ON ONTO" },
-  { type: "PREP", words: "IN INTO INSIDE" },
-  { type: "PREP", words: "WITH USING" },
-  { type: "PREP", words: "UNDER BELOW BENEATH UNDERNEATH" },
-  { type: "DIRECTION", words: "NORTH N" },
-  { type: "DIRECTION", words: "SOUTH S" },
-  { type: "DIRECTION", words: "EAST E" },
-  { type: "DIRECTION", words: "WEST W" },
-  { type: "DIRECTION", words: "NORTHWEST NW" },
-  { type: "DIRECTION", words: "NORTHEAST NE" },
-  { type: "DIRECTION", words: "SOUTWEST SW" },
-  { type: "DIRECTION", words: "SOUTHEAST E" },
-  { type: "DIRECTION", words: "UP U" },
-  { type: "DIRECTION", words: "DOWN D" },
-];
+  { type: 'VERB', words: 'CLEAR CLR WIPE' },
+  { type: 'VERB', words: 'HELLO HI YO HOWDY' },
+  { type: 'VERB', words: 'YELL SHOUT' },
+  { type: 'VERB', words: 'LOOK L' },
+  { type: 'NOUN', words: 'LAMP LIGHT LANTERN' },
+  { type: 'PREP', words: 'AT TO' },
+  { type: 'PREP', words: 'ON ONTO' },
+  { type: 'PREP', words: 'IN INTO INSIDE' },
+  { type: 'PREP', words: 'WITH USING' },
+  { type: 'PREP', words: 'UNDER BELOW BENEATH UNDERNEATH' },
+  { type: 'DIRECTION', words: 'NORTH N' },
+  { type: 'DIRECTION', words: 'SOUTH S' },
+  { type: 'DIRECTION', words: 'EAST E' },
+  { type: 'DIRECTION', words: 'WEST W' },
+  { type: 'DIRECTION', words: 'NORTHWEST NW' },
+  { type: 'DIRECTION', words: 'NORTHEAST NE' },
+  { type: 'DIRECTION', words: 'SOUTWEST SW' },
+  { type: 'DIRECTION', words: 'SOUTHEAST E' },
+  { type: 'DIRECTION', words: 'UP U' },
+  { type: 'DIRECTION', words: 'DOWN D' },
+]
 
 export const synonmys_map = () => {
-  let wordMap = new Map();
+  let wordMap = new Map()
 
   for (let obj of synonyms) {
-    const wordType = obj.type;
-    const words = obj.words.split(" ");
-    const keyWord = words[0];
+    const wordType = obj.type
+    const words = obj.words.split(' ')
+    const keyWord = words[0]
     for (let word of words) {
-      wordMap.set(word, { type: wordType, keyWord: keyWord });
+      wordMap.set(word, { type: wordType, keyWord: keyWord })
     }
   }
-  return wordMap;
-};
-
-export const stopWords = ["THE", "A", "AN"];
+  return wordMap
+}
 
 const WEST_OF_HOUSE = {
-  name: "WEST-OF-HOUSE",
-  in: "ROOMS",
-  desc: "West of House",
+  name: 'WEST-OF-HOUSE',
+  in: 'ROOMS',
+  desc: 'West of House',
   exits: [
-    { dir: "NORTH", type: "UEXIT", to: "NORTH-OF-HOUSE" },
-    { dir: "SOUTH", type: "UEXIT", to: "SOUTH-OF-HOUSE" },
+    { dir: 'NORTH', type: 'UEXIT', to: 'NORTH-OF-HOUSE' },
+    { dir: 'SOUTH', type: 'UEXIT', to: 'SOUTH-OF-HOUSE' },
   ],
-  action: "",
-};
+  action: '',
+}
 
 const rooms = [
   {
-    name: "WEST-OF-HOUSE",
-    in: "ROOMS",
-    desc: "West of House",
+    name: 'WEST-OF-HOUSE',
+    in: 'ROOMS',
+    desc: 'West of House',
     exits: [
-      { dir: "NORTH", type: "UEXIT", to: "NORTH-OF-HOUSE" },
-      { dir: "SOUTH", type: "UEXIT", to: "SOUTH-OF-HOUSE" },
+      { dir: 'NORTH', type: 'UEXIT', to: 'NORTH-OF-HOUSE' },
+      { dir: 'SOUTH', type: 'UEXIT', to: 'SOUTH-OF-HOUSE' },
     ],
-    action: "",
+    action: '',
   },
   {
-    name: "NORTH-OF-HOUSE",
-    in: "ROOMS",
-    desc: "North of House",
+    name: 'NORTH-OF-HOUSE',
+    in: 'ROOMS',
+    desc: 'North of House',
     exit: [
-      { dir: "WEST", type: "UEXIT", to: "WEST-OF-HOUSE" },
-      { dir: "EAST", type: "UEXIT", to: "EAST-OF-HOUSE" },
+      { dir: 'WEST', type: 'UEXIT', to: 'WEST-OF-HOUSE' },
+      { dir: 'EAST', type: 'UEXIT', to: 'EAST-OF-HOUSE' },
     ],
-    action: "",
+    action: '',
   },
   {
-    name: "EAST-OF-HOUSE",
-    in: "ROOMS",
-    desc: "East of House",
+    name: 'EAST-OF-HOUSE',
+    in: 'ROOMS',
+    desc: 'East of House',
     exits: [
-      { dir: "NORTH", type: "UEXIT", to: "NORTH-OF-HOUSE" },
-      { dir: "SOUTH", type: "UEXIT", to: "SOUTH-OF-HOUSE" },
+      { dir: 'NORTH', type: 'UEXIT', to: 'NORTH-OF-HOUSE' },
+      { dir: 'SOUTH', type: 'UEXIT', to: 'SOUTH-OF-HOUSE' },
     ],
-    action: "",
+    action: '',
   },
   {
-    name: "SOUTH-OF-HOUSE",
-    in: "ROOMS",
-    desc: "South of House",
+    name: 'SOUTH-OF-HOUSE',
+    in: 'ROOMS',
+    desc: 'South of House',
     exit: [
-      { dir: "WEST", type: "UEXIT", to: "WEST-OF-HOUSE" },
-      { dir: "EAST", type: "UEXIT", to: "EAST-OF-HOUSE" },
+      { dir: 'WEST', type: 'UEXIT', to: 'WEST-OF-HOUSE' },
+      { dir: 'EAST', type: 'UEXIT', to: 'EAST-OF-HOUSE' },
     ],
-    action: "",
+    action: '',
   },
-];
+]
 
 const objects = [
-  { name: "NOT-HERE-OBJECT", desc: "such thing", synonyms: "", action: "" },
+  { name: 'NOT-HERE-OBJECT', desc: 'such thing', synonyms: '', action: '' },
   {
-    name: "STAIRS",
-    desc: "stairs",
-    synonyms: "STAIRS STEPS STAIRCASE STAIRWAY",
-    action: "",
+    name: 'STAIRS',
+    desc: 'stairs',
+    synonyms: 'STAIRS STEPS STAIRCASE STAIRWAY',
+    action: '',
   },
-  { name: "ME", synonyms: "ME MYSELF SELF" },
-];
+  { name: 'ME', synonyms: 'ME MYSELF SELF' },
+]
 
-const routines = [{ name: "V-HELLO", action: "SAY" }];
+const routines = [{ name: 'V-HELLO', action: 'SAY' }]
